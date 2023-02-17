e4fa70cb95b462bbfad3200d74ab1d2cf1949643
Nick Condron <ncondron42@gmail.com>
2023-02-16 23:22:34 -0500

    Update script

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
+// to run this script: `node <script.js> <replay.slp>
 const util = require("util");
 
 const { SlippiGame } = require("@slippi/slippi-js");
 
-//const game = new SlippiGame("long_name_tag.slp");
 const game = new SlippiGame(process.argv[2]);
 
 // Get game settings – stage, characters, etc
 const settings = game.getSettings();
+const gecko = game.getGeckoList();
+const frames = game.getFrames();
+const end = game.getGameEnd();
 const metadata = game.getMetadata();
-//const frames = game.getFrames();
 
 console.log(settings.randomSeed);
 console.log(util.inspect(metadata.playedOn, false, null, true));
